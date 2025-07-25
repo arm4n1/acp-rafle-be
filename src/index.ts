b986b9139a17493006ece9c7f533ed1efeb44433
arm4n1 <armanraygeki18@gmail.com>
2025-07-25 21:46:40 +0800

    repom ini baru sampai register API

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./routes/api";
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.use('/api', router)
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
---
+++ src/routes/api.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import authController from "../controller/auth.controller";
+
+const router = express.Router();
+
+
+// router.post("/auth/register", authController.register);
+router.post("/auth/register", authController.register)
+
+export default router;
