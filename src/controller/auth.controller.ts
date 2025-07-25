01cf5658361b704961e807e21d204987ae74fd6c
arm4n1 <armanraygeki18@gmail.com>
2025-07-26 00:20:20 +0800

    menambahkan login api nich

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+
 type TRegister = {
   fullName: string;
   username: string;
   email: string;
   password: string;
   confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
 };
 
 const registerValidateSchema = Yup.object({
@@ -31,20 +39,70 @@ export default {
         confirmPassword,
       });
 
+      const result = await UserModel.create({
+        fullName,
+        email,
+        username,
+        password,
+      });
+
       res.status(200).json({
         message: "Success registration",
-        data: {
-            fullName,
-            username,
-            email,
-        },
+        data: result,
       });
     } catch (error) {
-        const err = error as unknown as Error;
-        res.status(400).json({
-            message: err.message,
-            data: null,
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      // ambil data user berdasarkan "identifier" -> emal dan username
+
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
         });
-    };
+      }
+      // validasi password
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+      }
+
+      res.status(200).json({
+        message: "Login Success",
+        data: userByIdentifier,
+      });
+      
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
   },
 };
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 // router.post("/auth/register", authController.register);
 router.post("/auth/register", authController.register)
+router.post("/auth/login", authController.login)
 
 export default router;
